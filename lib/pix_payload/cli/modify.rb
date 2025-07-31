4603ae88822dede63213affafc462eaec509f4f6
Thierry Castro <thierrycastro555@gmail.com>
2025-07-31 12:50:35 -0300

    feat: cli comando modify

---
+++ lib/pix_payload/cli/modify.rb
@@ -0,0 +1,31 @@
+require_relative "../modifier"
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Uso: pix_payload modify --payload CODIGO [--valor NOVO_VALOR] [--txid NOVO_TXID] ..."
+
+  opts.on("--payload PAYLOAD", "Código original") { |v| options[:payload] = v }
+  opts.on("--valor VALOR", Float, "Novo valor") { |v| options[:valor] = v }
+  opts.on("--txid TXID", "Novo TxID") { |v| options[:txid] = v }
+  opts.on("--descricao DESC", "Nova descrição") { |v| options[:descricao] = v }
+  opts.on("--nome NOME", "Novo nome") { |v| options[:nome] = v }
+  opts.on("--cidade CIDADE", "Nova cidade") { |v| options[:cidade] = v }
+  opts.on("--chave CHAVE", "Nova chave") { |v| options[:chave] = v }
+  opts.on("-h", "--help", "Ajuda") do
+    puts opts
+    exit
+  end
+end.parse!
+
+if options[:payload]
+  novo_payload = PixPayload::Modifier.alterar(options.delete(:payload), options)
+  if novo_payload
+    puts novo_payload
+  else
+    puts "Erro: payload inválido ou não pôde ser modificado"
+    exit 1
+  end
+else
+  puts "Erro: informe --payload"
+  exit 1
+end
