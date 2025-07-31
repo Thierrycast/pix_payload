19187ba82580cdabe44abe7f667efa0e9a940f84
Thierry Castro <thierrycastro555@gmail.com>
2025-07-31 11:59:35 -0300

    feat: permite alterar campos especificos de umpayload ja existente

--- lib/pix_payload.rb
+++ lib/pix_payload.rb
@@ -3,3 +3,4 @@
 require_relative "pix_payload/version"
 require_relative "pix_payload/generator"
 require_relative "pix_payload/parser"
+require_relative "pix_payload/modifier"
---
+++ lib/pix_payload/modifier.rb
@@ -0,0 +1,21 @@
+require_relative "version"
+
+class Modifier
+  def self.alterar(payload, campos = {})
+    dados_originais = PixPayload::Parser.parse_payload(payload)
+    if dados_originais.empty? || dados_originais[:chave].nil? || dados_originais[:nome].nil? || dados_originais[:cidade].nil?
+      return nil
+    end
+
+    dados_finais = dados_originais.merge(campos.transform_keys(&:to_sym))
+
+    PixPayload::Generator.generate_payload(
+      chave: dados_finais[:chave],
+      nome: dados_finais[:nome],
+      cidade: dados_finais[:cidade],
+      valor: dados_finais[:valor],
+      txid: dados_finais[:txid],
+      descricao: dados_finais[:descricao]
+    )
+  end
+end
